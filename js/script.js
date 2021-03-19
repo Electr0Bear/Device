4e67dd9c8c0aceef53127eff12d770efc56658f8
Electr0Bear <dmitry.kn@protonmail.com>
2021-03-19 13:11:40 +0300

    переделывает работу скрипта на слайдере

--- js/script.js
+++ js/script.js
@@ -1,24 +1,56 @@
-const firstSlideButton = document.querySelector('.first-slide-button');
-const secondSlideButton = document.querySelector('.second-slide-button');
-const thirdSlideButton = document.querySelector('.third-slide-button');
-const firstSlide = document.querySelector('.first-slide');
-const secondSlide = document.querySelector('.second-slide');
-const thirdSlide = document.querySelector('.third-slide');
+const sliderSelectorSection = document.querySelector('.slider-selectors');
+const sliderSelectorArray = sliderSelectorSection.querySelectorAll('.slider-selector');
+const sliderItemArray = document.querySelectorAll('.slider-item');
 
-firstSlideButton.addEventListener('click', evt => {
-  firstSlide.classList.add('slider-show');
-  secondSlide.classList.remove('slider-show');
-  thirdSlide.classList.remove('slider-show');
+for (let i = 0; i < sliderSelectorArray.length; i++) {
+  sliderSelectorArray[i].addEventListener('click', evt => {
+    evt.preventDefault();
+    console.log(`нажата кнопка ${i+1}`);
+    if (!sliderItemArray[i].classList.contains('slider-show')) {
+      //console.log(sliderItemArray[i]);
+      for (let j = 0; j < sliderItemArray.length; j++ ) {
+        if (sliderItemArray[j].classList.contains('slider-show')) {
+          sliderItemArray[j].classList.toggle('slider-show');
+          console.log('скрывает слайд:');
+          console.log(sliderItemArray[j]);
+        }
+      }
+      sliderItemArray[i].classList.toggle('slider-show');
+      console.log('показывает слайд:');
+      console.log(sliderItemArray[i]);
+    }
+  });
+}
+
+
+
+
+
+const deliveryButton = document.querySelector('.delivery-button');
+const guaranteeButton = document.querySelector('.guarantee-button');
+const creditButton = document.querySelector('.credit-button');
+const deliveryInfo = document.querySelector('.delivery-info');
+const guaranteeInfo = document.querySelector('.guarantee-info');
+const creditInfo = document.querySelector('.credit-info');
+
+deliveryButton.addEventListener('click', evt => {
+  evt.preventDefault();
+  deliveryInfo.classList.add('del-gr-cr-show');
+  guaranteeInfo.classList.remove('del-gr-cr-show');
+  creditInfo.classList.remove('del-gr-cr-show');
 });
 
-secondSlideButton.addEventListener('click', evt => {
-  firstSlide.classList.remove('slider-show');
-  secondSlide.classList.add('slider-show');
-  thirdSlide.classList.remove('slider-show');
+guaranteeButton.addEventListener('click', evt => {
+  evt.preventDefault();
+  deliveryInfo.classList.remove('del-gr-cr-show');
+  guaranteeInfo.classList.add('del-gr-cr-show');
+  creditInfo.classList.remove('del-gr-cr-show');
 });
 
-thirdSlideButton.addEventListener('click', evt => {
-  firstSlide.classList.remove('slider-show');
-  secondSlide.classList.remove('slider-show');
-  thirdSlide.classList.add('slider-show');
-});
+creditButton.addEventListener('click', evt => {
+  evt.preventDefault();
+  deliveryInfo.classList.remove('del-gr-cr-show');
+  guaranteeInfo.classList.remove('del-gr-cr-show');
+  creditInfo.classList.add('del-gr-cr-show');
+})
+
